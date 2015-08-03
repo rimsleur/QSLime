c75c6d1c3b9388619d26183b9a460239454a61ec
rimsleur <rimsleur@gmail.com>
2015-08-03 21:58:25 +0300

    Подготовка тестового примера 1005

---
+++ testcase/1005/fill.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+cd `cat ../path`
+./qlinkage -c 'кто'
+./qlinkage -c 'что'
+./qlinkage -c 'чего'
+./qlinkage -c 'какое'
+./qlinkage -c 'на-сколько'
+./qconcept -c 1 'увеличивать'
+./qconcept -c 1 'иметь'
+./qconcept -c 2 'ты'
+./qconcept -c 2 'значение'
+./qconcept -c 2 'поле'
+./qconcept -c 2 'имя'
+./qconcept -c 2 'testfield1'
