c3cdcd9449a819281732858e7ec0ee4645715990
rimsleur <rimsleur@gmail.com>
2015-08-03 21:34:59 +0300

    Подготовка тестового примера 1004

--- testcase/1004/fill.sh
+++ testcase/1004/fill.sh
@@ -11,3 +11,5 @@ CONCEPT1=`./qconcept -m`
 ./qconcept -c 7
 CONCEPT2=`./qconcept -m`
 ./qsl2qsl -c "#$CONCEPT1 ?что быть ?чем #$CONCEPT2"
+PREV_LINE=0
+./qconcept -a $CONCEPT2 $PREV_LINE 'ты ?кто увеличить ?что значение ?чего (=поле ?что иметь ?что имя ?какое testfield1) ?на-сколько 1'
