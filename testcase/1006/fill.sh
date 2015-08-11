e8a82e87212c1540d0c2af020ce88bf798d051fc
rimsleur <rimsleur@gmail.com>
2015-08-11 17:13:18 +0300

    Подготовка тестового примера 1006

---
+++ testcase/1006/fill.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+cd `cat ../path`
+./qlinkage -c 'кто'
+./qlinkage -c 'что'
+./qlinkage -c 'чем'
+./qlinkage -c 'какое'
+./qconcept -c 1 'выполнять'
+./qconcept -c 1 'иметь'
+./qconcept -c 1 'быть'
+./qconcept -c 2 'ты'
+./qconcept -c 2 'процедура'
+./qconcept -c 2 'имя'
+./qconcept -c 2 'TestProcedure1'
+./qconcept -c 5
+CONCEPT1=`./qconcept -m`
+./qsl2qsl -c "#$CONCEPT1 ?что быть ?чем процедура"
+./qsl2qsl -c "#$CONCEPT1 ?что иметь ?что имя ?какое TestProcedure1"
